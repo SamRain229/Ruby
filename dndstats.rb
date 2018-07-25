0ad61e4e53add4bbab300f491dd6d78e5eb8957e
SamRain229 <joshuakmeadows@gmail.com>
2018-07-25 18:28:58 -0400

    changed the method name

--- dndstats.rb
+++ dndstats.rb
@@ -1,6 +1,6 @@
 #Generates 4 random numbers and puts them in an array (the rand is +1 cuz random in ruby starts at 0)
-def roller
-  4.times.map{Random.rand(6) +1 } #=> [4, 2, 6, 8]
+def dndStats
+  4.times.map{Random.rand(6) + 1}
 end
 #deletes the lowest number in the array
 def remove_it(num)
@@ -8,9 +8,10 @@ def remove_it(num)
   num
 end
 #does this 6 times
+print "your stats are "
 x = 0
 until x == 6
-  print remove_it(roller).inject(0, :+) #the inject puts + inbetween each number in the arrray
+  print remove_it(dndStats).inject(0, :+) #the inject puts + inbetween each number in the arrray
   print " "
   x += 1
 end
