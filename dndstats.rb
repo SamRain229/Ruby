c6c18b4c7e8ce1da755dfbc365ea5841ca37ed2e
SamRain229 <joshuakmeadows@gmail.com>
2018-07-27 19:00:49 -0400

    Refactoring

--- dndstats.rb
+++ dndstats.rb
@@ -1,3 +1,4 @@
+require 'numbers_in_words'
 #Generates 4 random numbers and puts them in an array (the rand is +1 cuz random in ruby starts at 0)
 def dndStats
   4.times.map{Random.rand(6) + 1}
@@ -12,20 +13,34 @@ def statPrint
   print "your stats are "
   x = 0
     until x == 6
-        print remove_it(dndStats).inject(0, :+) #the inject puts + inbetween each number in the arrray
+        print remove_it(dndStats).sum
         print " "
         x += 1
     end
 end
 
 puts "How many Players do you have?"
-j = gets.chomp.to_i
-x = 0
-y = 0
-until x == j
-  y += 1
-  print "user #{y.to_i} "
-  print statPrint
-  puts ""
-  x += 1
+j = gets.chomp
+sleep 1
+ puts "\e[H\e[2J"
+if j.is_a? String
+  x = 0
+  y = 0
+    until x == NumbersInWords.in_numbers(j)
+      y += 1
+        print "Character #{y.to_i} "
+        print statPrint
+        puts "\n",""
+        x += 1
+      end
+else
+      x = 0
+      j.to_i
+    until x == j
+      y += 1
+        print "Character #{y.to_i} "
+        print statPrint
+        puts "\n",""
+        x += 1
+    end
 end
